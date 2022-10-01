67dc6fe1faf955e611a3eab2e1726a7365029749
Rudi <vogelrj@mail.uc.edu>
2022-09-30 21:36:02 -0400

    Initial commit

---
+++ src/Ass01_RockPaperScissors.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class Ass01_RockPaperScissors {
+    public static void main(String[] args) {
+        String PlayerA = "";
+        String PlayerB = "";
+        String GameResp = "";
+        boolean BlockVal = true;
+        boolean newGame = false;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Do you want to play Rock, Scissors, Paper? [Y/N]");
+        do {
+            GameResp = in.nextLine();
+            if (GameResp.equalsIgnoreCase("Y")) {
+                BlockVal = true;
+            }
+            else if (GameResp.equalsIgnoreCase("N")) {
+                System.out.println("Okay. Maybe next time. ");
+                System.exit(0);
+            }
+            else {
+                System.out.println("Invalid Entry. Please select [Y/N]");
+                BlockVal = false;
+            }
+        }
+        while (!BlockVal);
+        do {
+                System.out.println("You can be Player A. What is your move Rock [R], Scissors [S] or Paper [P]? ");
+                do {
+                    PlayerA = in.nextLine();
+                    if (PlayerA.matches("(?i)R|P|S")) {
+                        BlockVal = true;
+                    } else {
+                        System.out.println("Invalid Selection [" + PlayerA + "]. Please select [R/P/S].");
+                        BlockVal = false;
+                    }
+                }
+                while (!BlockVal);
+                System.out.println("Next up is Player B. What is your move Rock [R], Scissors [S] or Paper [P]? ");
+                do {
+                    PlayerB = in.nextLine();
+                    if (PlayerB.matches("(?i)R|P|S")) {
+                        BlockVal = true;
+                    } else {
+                        System.out.println("Invalid Selection [" + PlayerB + "]. Please select [R/P/S].");
+                        BlockVal = false;
+                    }
+                }
+                while (!BlockVal);
+                if (PlayerA.equalsIgnoreCase("R")) {
+                    if (PlayerB.equalsIgnoreCase("R")) {
+                        System.out.println("You both chose Rock. It's a Tie!");
+                    } else if (PlayerB.equalsIgnoreCase("P")) {
+                        System.out.println("Paper covers Rock. Player B wins!");
+                    } else {
+                        System.out.println("Rock breaks Scissors. Player A wins!");
+                    }
+                } else if (PlayerA.equalsIgnoreCase("S")) {
+                    if (PlayerB.equalsIgnoreCase("R")) {
+                        System.out.println("Rock breaks Scissors. Player B wins!");
+                    } else if (PlayerB.equalsIgnoreCase("P")) {
+                        System.out.println("Scissors cut Paper. Player A wins!");
+                    } else {
+                        System.out.println("You both chose Scissors. It's a Tie!");
+                    }
+                } else {
+                    if (PlayerB.equalsIgnoreCase("R")) {
+                        System.out.println("Paper covers rock. Player A wins!");
+                    } else if (PlayerB.equalsIgnoreCase("P")) {
+                        System.out.println("You both chose Paper. It's a Tie!");
+                    } else {
+                        System.out.println("Scissors cut Paper. Player B wins!");
+                    }
+                }
+                System.out.println("Do you want to play again? [Y/N]");
+                GameResp = in.nextLine();
+                if (GameResp.equalsIgnoreCase("y")) {
+                    newGame = true;
+                } else if (GameResp.equalsIgnoreCase("n")) {
+                    System.out.println("Thanks for Playing!");
+                    System.exit(0);
+                }
+            }
+            while (newGame = true);
+        }
+    }
